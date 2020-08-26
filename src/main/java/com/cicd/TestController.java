f7a54092e2b97bc1ce9b4f407be2c1098f301efd
vimrama <r_vimalsree@yahoo.com>
2020-08-26 16:30:41 +0530

    json rest api

---
+++ src/main/java/com/cicd/TestController.java
@@ -0,0 +1,22 @@
+package com.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+	@GetMapping("/employee")
+	public Employee firstPage() {
+
+		Employee emp = new Employee();
+		emp.setName("emp1");
+		emp.setDesignation("manager");
+		emp.setEmpId("1");
+		emp.setSalary(3000);
+
+		return emp;
+	}
+
+}
